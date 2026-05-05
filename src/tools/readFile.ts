7ee5a199ef031faa9116ad919cc08645e4299a05
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2026-05-05 22:55:36 +0530

    Tools archived.

--- src/tools/readFile.ts
+++ src/tools/readFile.ts
@@ -1,32 +1,32 @@
-import fs from "node:fs/promises";
-import { Tool } from "./types";
+// import fs from "node:fs/promises";
+// import { Tool } from "./types";
 
-export const readFileTool: Tool = {
-    name: "read_file",
-    definition: {
-        type: "function",
-        name: "read_file",
-        description: "Read the contents of a file from the user's current project.",
-        parameters: {
-            type: "object",
-            properties: {
-                path: {
-                    type: "string",
-                    description: "The relative path of the file to read.",
-                },
-            },
-            required: ["path"],
-            additionalProperties: false,
-        },
-        strict: true,
-    },
-    execute: async (args) => {
-        let toolResult
-        try {
-            toolResult = await fs.readFile(args.path, "utf-8");
-        } catch (err) {
-            toolResult = `Error reading file: ${(err as Error).message}`;
-        }
-        return toolResult
-    }
-}
+// export const readFileTool: Tool = {
+//     name: "read_file",
+//     definition: {
+//         type: "function",
+//         name: "read_file",
+//         description: "Read the contents of a file from the user's current project.",
+//         parameters: {
+//             type: "object",
+//             properties: {
+//                 path: {
+//                     type: "string",
+//                     description: "The relative path of the file to read.",
+//                 },
+//             },
+//             required: ["path"],
+//             additionalProperties: false,
+//         },
+//         strict: true,
+//     },
+//     execute: async (args) => {
+//         let toolResult
+//         try {
+//             toolResult = await fs.readFile(args.path, "utf-8");
+//         } catch (err) {
+//             toolResult = `Error reading file: ${(err as Error).message}`;
+//         }
+//         return toolResult
+//     }
+// }
